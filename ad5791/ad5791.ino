43d4ce8f0b3e6c9650b4cbf9bb0f2f70f8ea9fb9
skyguy-me <44331390+skyguy-me@users.noreply.github.com>
2019-07-03 19:00:18 +0800

    updates

---
+++ ad5791/ad5791.ino
@@ -0,0 +1,35 @@
+#include "src/ad57X1.h"
+
+#define CS_AD5791              7
+
+AD5791 ad5791(CS_AD5791, &SPI);
+// AD5781 pidDac(CS_AD5781, &SPI1);   // Use SPI1 on MCUs, that have multiple SPI controllers (e.g. Teensy)
+// AD5781 pidDac(CS_AD5781, &SPI, 30*1000*1000);    // Use an SPI clock frequency of 30 MHz, the maximum specified for the DAC.
+                                                    // If you experience trasmit errors, this might be due to your board layout.
+
+void setup() {
+  ad5791.begin();   // Set the pin modes
+  SPI.begin();
+  //ad5791.setOffsetBinaryEncoding(true);   // Set the input encoding to offset binary. Default is 2s complement (false).
+ 
+  //ad5781.setReferenceInputRange(true);   // Enable only if the reference voltage span is greater than 10 V, Default: false.
+
+  //ad5781.setOutputClamp(true);    // Enable to clamp the output to GND using a 6k resistor
+  //ad5781.updateControlRegister()  // Call this function after changing the controll register, to update the settings
+
+  //ad5781.setTristateMode(true);   // Disconnect the output (output will be tri-state)
+  //ad5781.updateControlRegister()  // Call this function after changing the controll register, to update the settings
+
+  //ad5781.setInternalAmplifier(true) // Enable the internal amplifier. This setup allows connecting an external amplifier in a gain of 2 configuration. See the datasheet for details.
+
+  ad5791.enableOutput();    // Turn on the DAC. After startup the output will be clamped to GND and disconnected (tri-state mode)
+                            // No need to call updateControlRegister(), because this is a convenience function, which does all that for you.
+ SPI.transfer(0x2F0001);
+  //ad5791.setValue(0x3FFFF); // Set to full scale output
+}
+
+void loop() {
+ ad5791.setValue(0x3FFFF); // Set to full scale output
+
+ 
+}
